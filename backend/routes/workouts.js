e06c81f779f163b08654c052d813a08ed5d4fa7a
Tjay05 <tjayolatayo@gmail.com>
2025-05-07 23:12:37 +0100

    Edit comment in workout route

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router();
 
-// require auth for all workout routes
+// require authentication for all workout routes
 router.use(requireAuth);
 
 // GET all workouts
