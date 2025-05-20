6a6f2de86f5404c6707a413d5b60b035ee5ed2f9
Tjay05 <tjayolatayo@gmail.com>
2025-05-20 20:29:44 +0100

    Protect react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {
   createBrowserRouter,
   Route,
   createRoutesFromElements,
-  RouterProvider
+  RouterProvider,
 } from 'react-router-dom';
 
 // pages & components
@@ -10,14 +10,28 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import RequireAuth from './components/RequireAuth';
+import RedirectIfAuth from './components/RedirectIfAuth';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route element={<Navbar/>}>
-        <Route path='/' element={<Home/>} />
-        <Route path='login' element={<Login/>} />
-        <Route path='signup' element={<Signup/>} />
+        <Route path='/' element={
+          <RequireAuth>
+            <Home/>
+          </RequireAuth>
+        } />
+        <Route path='login' element={
+          <RedirectIfAuth>
+            <Login/>
+          </RedirectIfAuth>
+        } />
+        <Route path='signup' element={
+          <RedirectIfAuth>
+            <Signup/>
+          </RedirectIfAuth>
+        } />
       </Route>
     </Route>
   )
