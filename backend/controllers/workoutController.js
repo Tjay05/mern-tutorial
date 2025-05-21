0faf6dc94378b470b9bd86a359e01d311d74a3a6
Tjay05 <tjayolatayo@gmail.com>
2025-05-21 08:53:15 +0100

    comment user id temporally

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 
 // get all workouts
 const getWorkouts = async (req, res) => {
-  const user_id = req.user._id;
+  // TEMPORAL
+  // const user_id = req.user._id;
 
-  const workouts = await Workout.find({ user_id }).sort({createdAt: -1});
+  // const workouts = await Workout.find({ user_id }).sort({createdAt: -1});
+  const workouts = await Workout.find();
 
   res.status(200).json(workouts);
 }
