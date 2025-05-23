9e3c367bc2c0f480b95520ab25bbc6c5a8bf0bc0
Tjay05 <tjayolatayo@gmail.com>
2025-05-23 16:23:41 +0100

    Wrap modal context around app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+// Context Providers
 import { WorkoutsContextProvider } from './context/WorkoutContext';
 import { AuthContextProvider } from './context/AuthContext';
+import { ModalContextProvider } from './context/ModalContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
       <WorkoutsContextProvider>
-        <App />
+        <ModalContextProvider>
+          <App />
+        </ModalContextProvider>
       </WorkoutsContextProvider>
     </AuthContextProvider>
   </StrictMode>,
