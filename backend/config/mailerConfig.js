3c599226cf72bf1d1530efd94e89bf9427b6b18b
Tjay05 <tjayolatayo@gmail.com>
2025-06-02 12:01:41 +0100

    Move cloudinary and nodemailer to config folder

---
+++ backend/config/mailerConfig.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+
+// Nodemailer credentials
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.AUTH_EMAIL,
+    pass: process.env.AUTH_PASS,
+  },
+});
+
+module.exports = transporter;
