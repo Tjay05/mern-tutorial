84479e89ea5faa9956ff4945ab15806dc45caf33
Tjay05 <tjayolatayo@gmail.com>
2025-06-07 21:06:15 +0100

    Edit component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           <WorkoutDetails key={workout._id} workout={workout} />
         ))}
       </div>
-      <WorkoutForm/>
+      {/* <WorkoutForm/> */}
     </div>
   );
 }
