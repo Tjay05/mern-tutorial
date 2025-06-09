351c8c776ee29f4030448e9757921ebc3cab30f9
Tjay05 <tjayolatayo@gmail.com>
2025-06-09 20:10:29 +0100

    Destructure usesignup import

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -4,7 +4,7 @@ import { useSignup } from "../hooks/useSignup"
 const Signup = ({ onSent }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const {signup, isLoading, error} = useSignup()
+  const { signup, isLoading, error } = useSignup()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
