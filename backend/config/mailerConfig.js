e043302c4f4f90ab570e9ff45943c85aea2e4a5a
Tjay05 <tjayolatayo@gmail.com>
2025-10-21 14:20:36 +0100

    Edit transporter credentials

--- backend/config/mailerConfig.js
+++ backend/config/mailerConfig.js
@@ -2,9 +2,10 @@ const nodemailer = require('nodemailer');
 
 // Nodemailer credentials
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
+  // host: "smtp.gmail.com",
+  // port: 465,
+  // secure: true,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
