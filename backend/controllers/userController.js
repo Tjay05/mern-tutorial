b372a7bdac95c36be12f9067f9a0e386b432e694
Tjay05 <tjayolatayo@gmail.com>
2025-10-22 14:32:45 +0100

    Add dotenv config

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const UserOTPVerification = require('../models/userOtpVerificationModel');
 
