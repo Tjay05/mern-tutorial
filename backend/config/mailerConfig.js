a69e7031ea8e6637f8480198fc25781a6d41f289
Tjay05 <tjayolatayo@gmail.com>
2025-10-23 21:47:38 +0100

    Change auth to Oauth2 and google apis

--- backend/config/mailerConfig.js
+++ backend/config/mailerConfig.js
@@ -1,18 +1,36 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 
-// Nodemailer credentials
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  // host: "smtp.gmail.com",
-  // port: 465,
-  // secure: true,
-  auth: {
-    type: "OAuth2",
-    user: process.env.AUTH_EMAIL,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  },
-});
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+)
 
-module.exports = transporter;
+oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
+
+async function createTransporter() {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    // Nodemailer credentials
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.AUTH_EMAIL,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      },
+    });
+
+    return transporter;
+  } catch (error) {
+    console.error("error creating transporter:", error.message);
+    throw new Error('failed to create mail transporter')
+  }
+}
+
+
+module.exports = createTransporter;
