1e4d736280b760f06690283014b6d3fb5905febd
Tjay05 <tjayolatayo@gmail.com>
2025-10-23 16:54:56 +0100

    Change auth type to oauth2

--- backend/config/mailerConfig.js
+++ backend/config/mailerConfig.js
@@ -7,8 +7,11 @@ const transporter = nodemailer.createTransport({
   // port: 465,
   // secure: true,
   auth: {
+    type: "OAuth2",
     user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_PASS,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_CLIENT_REFRESH_TOKEN,
   },
 });
 
